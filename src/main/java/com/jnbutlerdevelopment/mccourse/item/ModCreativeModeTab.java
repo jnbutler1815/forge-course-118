f60d5ca7d34361a91246086ad9018be729d8153f
James Butler <jnbutler1815@gmail.com>
2022-06-07 10:57:37 -0500

    Added parchment entries in gradlebuild file.

---
+++ src/main/java/com/jnbutlerdevelopment/mccourse/item/ModCreativeModeTab.java
@@ -0,0 +1,13 @@
+package com.jnbutlerdevelopment.mccourse.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab COURSE_TAB = new CreativeModeTab("coursemodtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(MODItems.COBALT_INGOT.get());
+        }
+    };
+}
